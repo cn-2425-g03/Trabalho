9aa6aec5f1e0c59de35733cd4c234b0eae6203e3
LucasSilvaIT <lucas.silva.it@outlook.com>
2025-05-27 14:03:49 +0100

    feat: lookup-function added

---
+++ lookup-function/src/main/java/com/github/cn2425g03/lookup/Lookup.java
@@ -0,0 +1,34 @@
+package com.github.cn2425g03.lookup;
+
+import com.google.cloud.compute.v1.InstancesClient;
+import com.google.cloud.functions.HttpFunction;
+import com.google.cloud.functions.HttpRequest;
+import com.google.cloud.functions.HttpResponse;
+import com.google.gson.Gson;
+
+import java.io.BufferedWriter;
+import java.util.List;
+
+public class Lookup implements HttpFunction {
+
+    private final static String PROJECT_ID = "cn2425-t3-g03";
+    private final static String ZONE = "europe-southwest1-b";
+
+    @Override
+    public void service(HttpRequest httpRequest, HttpResponse httpResponse) throws Exception {
+
+        try (BufferedWriter writer = httpResponse.getWriter(); InstancesClient client = InstancesClient.create()) {
+
+            List<String> ips = client.list(PROJECT_ID, ZONE).getPage().getResponse().getItemsList().stream()
+                    .map(instance -> instance.getNetworkInterfaces(0).getAccessConfigs(0).getNatIP())
+                    .toList();
+
+            Gson gson = new Gson();
+            gson.toJson(ips);
+
+            writer.write(gson.toString());
+        }
+
+    }
+
+}
